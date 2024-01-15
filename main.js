968ac448fcf6da613eaf814948e4f07e4fd89291
stoffe-fe23 <kristoffer.bengtsson@gritacademy.se>
2024-01-15 21:41:29 +0100

    Grundstomme och koncept-demo

---
+++ main.js
@@ -0,0 +1,254 @@
+/*
+    Versionshantering slutprojekt (FE23)
+    Grupp 4
+
+    Main script of the page. Respond to user input. 
+*/
+
+import {
+    addChatMessage,
+    getChatMessages,
+    userLogin,
+    userLogoff,
+    userIsLoggedIn,
+    createNewUser,
+    getCurrentUserName,
+    getCurrentUserProfile,
+    userUpdateProfile,
+    setUserLoginCallback,
+    setUserLogoffCallback,
+} from './modules/api.js';
+
+
+// Configure function to run when a user has logged in
+setUserLoginCallback(userLoggedInCallback);
+
+// Configure function to run when the user has logged off
+setUserLogoffCallback(userLoggedOffCallback);
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// EXAMPLE: Create a new Message
+document.querySelector("#store-form").addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (userIsLoggedIn()) {
+        addChatMessage(document.querySelector("#store-value").value.trim()).then((newDoc) => {
+            console.log("New document", newDoc.path, newDoc.id, newDoc);
+            refreshMessages();
+        });
+    }
+});
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// EXAMPLE: Update Messages from database
+document.querySelector("#fetch-values").addEventListener("click", (event) => {
+    refreshMessages();
+});
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// EXAMPLE: Fetch the 20 most recent Messages from the database and display them
+function refreshMessages() {
+    getChatMessages(20).then((dbData) => {
+        const outputBox = document.querySelector("#output");
+        outputBox.innerHTML = "";
+
+
+        if ((dbData !== undefined) && (dbData !== null) && (typeof dbData == "object") && (Object.keys(dbData).length > 0)) {
+            for (const docName in dbData) {
+                outputBox.appendChild(newOutputMessage(dbData[docName], docName));
+            }
+        }
+
+        console.log("CHAT MESSAGES", dbData);
+    });
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// EXAMPLE: Build and return a Message HTML/DOM-element
+//          dataItem is an object containing the message data
+//          docName is the message-ID (i.e. document name in the database) used to uniquely identify this message
+function newOutputMessage(dataItem, docName) {
+    const dataContainer = document.createElement("div");
+    const dataDate = document.createElement("div");
+    const dataMessage = document.createElement("div");
+    const dataAuthor = document.createElement("div");
+
+    console.log("DATAITEM", docName, dataItem);
+    dataDate.classList.add("fetched-data-date");
+    dataMessage.classList.add("fetched-data-message");
+    dataAuthor.classList.add("fetched-data-author");
+    dataDate.innerText = ((dataItem.date.seconds !== undefined) && (dataItem.date.seconds !== null) ? timestampToDateTime(dataItem.date.seconds, false) : "Date missing");
+    dataMessage.innerText = ((dataItem.message !== undefined) && (dataItem.message.length > 0) ? dataItem.message : "No message");
+    dataAuthor.innerText = ((dataItem.authorname !== undefined) && (dataItem.authorname.length > 0) ? dataItem.authorname : "No name");
+
+    dataContainer.append(dataDate, dataMessage, dataAuthor);
+
+    // Store the messageID and authorID in an attribute for potential use in edit/delete ops
+    dataContainer.setAttribute("messageid", docName);
+    dataContainer.setAttribute("authorid", dataItem.authorid);
+
+    return dataContainer;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// EXAMPLE: Login form
+document.querySelector("#login-form").addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    if (!userIsLoggedIn()) {
+        const loginName = document.querySelector("#login-email").value.trim();
+        const loginPassword = document.querySelector("#login-password").value.trim();
+
+        userLogin(loginName, loginPassword).catch((error) => {
+            /*
+                If login fails errorCode may be set to one of the following:
+                 - auth/invalid-email: Thrown if the email address is not valid.
+                 - auth/user-disabled: Thrown if the user corresponding to the given email has been disabled.
+                 - auth/user-not-found: Thrown if there is no user corresponding to the given email.
+                 - auth/wrong-password: Thrown if the password is invalid for the given email, or the account corresponding to the email does not have a password set.
+                 - auth/invalid-credential: Also seems to be thrown if the specified user does not exist? 
+            */
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.error("LOGIN ERROR", errorMessage, errorCode);
+        });
+
+        event.currentTarget.reset();
+    }
+});
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// EXAMPLE: Log off button
+document.querySelector("#logoff-button").addEventListener("click", (event) => {
+    if (userIsLoggedIn()) {
+        userLogoff();
+    }
+});
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// EXAMPLE: Create new user form
+document.querySelector("#new-user-form").addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const newEmail = document.querySelector("#new-user-email").value.trim();
+    const newPassword = document.querySelector("#new-user-password").value.trim();
+    const newName = document.querySelector("#new-user-name").value.trim();
+
+    createNewUser(newEmail, newPassword, newName).catch((error) => {
+        /*
+            If an error occurs during creation, errorCode may be one of:
+             - auth/email-already-in-use: Thrown if there already exists an account with the given email address.
+             - auth/invalid-email: Thrown if the email address is not valid.
+             - auth/operation-not-allowed: Thrown if email/password accounts are not enabled.
+             - auth/weak-password: Specified password is too weak and disallowed
+        */
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log("USER CREATE ERROR", errorMessage, errorCode);
+    });
+
+    event.currentTarget.reset();
+});
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// EXAMPLE: Change user name button
+document.querySelector("#update-user").addEventListener("click", (event) => {
+    if (userIsLoggedIn()) {
+        const inputField = document.querySelector("#change-name-input");
+        inputField.value = getCurrentUserName();
+        inputField.focus();
+        inputField.select();
+        document.querySelector("#change-name-dialog").showModal();
+    }
+});
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// EXAMPLE: Change user name form
+document.querySelector("#change-name-form").addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (userIsLoggedIn()) {
+        const inputValue = document.querySelector("#change-name-input").value.trim();
+        const profileData = { displayName: inputValue };
+
+        userUpdateProfile(profileData).then((param) => {
+            getCurrentUserProfile().then((currUser) => {
+                console.log("PROFILE UPDATED", currUser, param);
+                document.querySelector("#change-name-dialog").close();
+                document.querySelector("#logged-in-email").innerHTML = `${getCurrentUserName()} <span>(${currUser.email})</span>`;
+            });
+        });
+    }
+});
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// EXAMPLE: This function is run when user login is completed
+function userLoggedInCallback(currUser) {
+    console.log("ANV INLOGG", currUser);
+
+    getCurrentUserProfile().then((currUser) => {
+        const loginForm = document.querySelector("#login-form");
+        const logoutBox = document.querySelector("#logged-in");
+        const userEmail = document.querySelector("#logged-in-email");
+        const userDate = document.querySelector("#logged-in-last");
+
+        userEmail.innerHTML = `${getCurrentUserName()} <span>(${currUser.email})</span>`;
+        userDate.innerText = `last login: ${currUser.lastLogin}`;
+        loginForm.classList.remove("show");
+        logoutBox.classList.add("show");
+        showMessageForm(true);
+    });
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// EXAMPLE: This function is run when user logoff is concluded.
+function userLoggedOffCallback() {
+    const loginForm = document.querySelector("#login-form");
+    const logoutBox = document.querySelector("#logged-in");
+    const userEmail = document.querySelector("#logged-in-email");
+    const userDate = document.querySelector("#logged-in-last");
+
+    userEmail.innerText = '';
+    userDate.innerText = '';
+    loginForm.classList.add("show");
+    logoutBox.classList.remove("show");
+    showMessageForm(false);
+    console.log("ANV. UTLOGG");
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// EXAMPLE: Toggle visible components depending on if a user is logged on or not
+function showMessageForm(isVisible) {
+    const storeForm = document.querySelector("#store-value-wrapper");
+    const storeButton = document.querySelector("#store-button");
+    const nameButton = document.querySelector("#update-user");
+    const userUserForm = document.querySelector("#new-user-form");
+
+    storeButton.disabled = !isVisible;
+    nameButton.disabled = !isVisible;
+    if (isVisible) {
+        storeForm.classList.add("show");
+        userUserForm.classList.remove("show");
+    }
+    else {
+        storeForm.classList.remove("show");
+        userUserForm.classList.add("show");
+    }
+
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// EXAMPLE: Convert a timestamp number to a displayable date string
+function timestampToDateTime(timestamp, isMilliSeconds = true) {
+    const dateObj = new Date(isMilliSeconds ? timestamp : timestamp * 1000);
+    return `${dateObj.toLocaleDateString('sv-SE')} ${dateObj.toLocaleTimeString('sv-SE')}`;
+}
--- modules/api.js
+++ modules/api.js
@@ -1,4 +1,325 @@
-/*  Funktioner för kommunikation med API */
+/*
+    Versionshantering slutprojekt (FE23)
+    Grupp 4
+
+    Functionality for retrieving and storing data from the Firebase API,
+    and managing user logins.
+*/
+
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js';
+
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged,
+    updateProfile
+} from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js';
+
+import {
+    getFirestore,
+    collection,
+    doc,
+    getDocs,
+    addDoc,
+    getDoc,
+    setDoc,
+    updateDoc,
+    Timestamp,
+    query,
+    orderBy,
+    limit,
+    where
+} from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore-lite.js';
 
 
 import { firebaseConfig } from './apiconfig.js';
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore(app);
+
+
+let currentUser = auth.currentUser;
+let userLoginCallback;
+let userLogoffCallback;
+
+
+/****************************************************************************************
+ * USER MANAGEMENT
+ ****************************************************************************************/
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// Set the callback function to run when a user has completed logging on.
+function setUserLoginCallback(callbackFunc) {
+    userLoginCallback = callbackFunc;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// Set the callback function to run when a user has completed logging off. 
+function setUserLogoffCallback(callbackFunc) {
+    userLogoffCallback = callbackFunc;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// Returns true if there is currently a user logged on, otherwise false.
+function userIsLoggedIn() {
+    return (currentUser !== null);
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// Update authenticated user status. Run relevant callback functions set with the 
+// setUserLoginCallback() and setUserLogoffCallback() functions. 
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        currentUser = user;
+        if (typeof userLoginCallback == "function") {
+            userLoginCallback();
+        }
+        console.log("CURRENT USER", currentUser);
+    }
+    else {
+        currentUser = null;
+        if (typeof userLogoffCallback == "function") {
+            userLogoffCallback();
+        }
+        console.log("NO USER", currentUser);
+    }
+});
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// Authenticate the current user. Use the onAuthStateChanged callback set with the 
+// setUserLoginCallback() function to respond when the login process has finished.
+async function userLogin(loginName, loginPassword) {
+    return signInWithEmailAndPassword(auth, loginName, loginPassword).then((userCredential) => {
+        currentUser = userCredential.user;
+        console.log("SIGNED IN", userCredential);
+    });
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// Log off the current user.  Use the onAuthStateChanged callback set with the 
+// setUserLogoffCallback() function to respond when the login process has finished.
+function userLogoff() {
+    signOut(auth).then(() => {
+        currentUser = null;
+        console.log("LOG OFF SUCCESSFUL");
+    }).catch((error) => {
+        console.error("LOG OFF FAILED", error);
+    });
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// Get the name of the current user or "No name" if no user is logged on or the user has
+// not set any display name. 
+function getCurrentUserName() {
+    if ((currentUser !== undefined) && (currentUser !== null) && getIsValidText(currentUser.displayName)) {
+        return (currentUser.displayName.length > 0 ? currentUser.displayName : "No name");
+    }
+    else {
+        return "No name";
+    }
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// Update profile data of the currently logged on user
+// The profileData parameter must be an object with properties for the profile fields to update.
+// Possible properties are: displayName, picture, color
+// Function returns a Promise for when the update is complete.
+async function userUpdateProfile(profileData) {
+    const authProfileData = {};
+    const userProfileData = {};
+
+    if (getIsValidText(profileData.displayName)) {
+        authProfileData.displayName = profileData.displayName;
+        userProfileData.username = profileData.displayName;
+    }
+    if (getIsValidText(profileData.picture)) {
+        authProfileData.photoURL = profileData.picture;
+        userProfileData.picture = profileData.picture;
+    }
+    if (getIsValidText(profileData.color)) {
+        userProfileData.picture = profileData.color;
+    }
+
+    // Update the user profile fields in the database
+    const docProfile = await getDoc(doc(db, "userprofiles", currentUser.uid));
+    if (docProfile.exists()) {
+        await updateDoc(doc(db, "userprofiles", currentUser.uid), userProfileData);
+    }
+    else {
+        userProfileData.userid = currentUser.uid;
+        await setDoc(doc(db, "userprofiles", currentUser.uid), userProfileData);
+    }
+
+    // Update user authentication data
+    return await updateProfile(auth.currentUser, authProfileData);
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// Retrieve the profile data of the currently logged on user
+// Function returns a Promise with the user profile data as callback parameter.
+async function getCurrentUserProfile() {
+    let userProfile = {};
+    if (currentUser !== null) {
+        userProfile = {
+            uid: currentUser.uid,
+            displayName: currentUser.displayName,
+            email: currentUser.email,
+            verified: currentUser.emailVerified,
+            phone: currentUser.phoneNumber,
+            picture: currentUser.photoURL,
+            color: '',
+            lastLogin: currentUser.metadata.lastSignInTime,
+        }
+
+        const docProfile = await getDoc(doc(db, "userprofiles", currentUser.uid));
+        if (docProfile.exists()) {
+            const docProfileData = docProfile.data();
+            if (getIsValidText(docProfileData.picture)) {
+                userProfile.picture = docProfileData.picture;
+            }
+            if (getIsValidText(docProfileData.color)) {
+                userProfile.color = docProfileData.color;
+            }
+        }
+    }
+    return userProfile;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// Add a new user account (login) to the system.
+// Returns a Promise with the credentials of the new user as callback parameter.
+async function createNewUser(userEmail, userPassword, userName) {
+    return createUserWithEmailAndPassword(auth, userEmail, userPassword).then((userCredential) => {
+        currentUser = userCredential.user;
+
+        if ((userName !== undefined) && (userName.length > 0)) {
+            userUpdateProfile({ displayName: userName }).then(() => {
+                userLoginCallback();
+            });
+
+        }
+        console.log("USER CREATED", userCredential);
+    });
+}
+
+
+/****************************************************************************************
+ * CHAT MESSAGES
+ ****************************************************************************************/
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// Retrieve up to messageLimit Messages from the database, in falling chronological order.
+// Function returns a Promise with an array of Message objects as callback parameter. 
+async function getChatMessages(messageLimit = 30) {
+    return dbGetCollectionDocuments(db, 'chatmeddelande', ["date", "desc"]).then((dbData) => {
+        const chatMessages = {};
+
+        if ((dbData !== undefined) && (dbData !== null) && Array.isArray(dbData) && (dbData.length > 0)) {
+            for (const dataItem of dbData) {
+                chatMessages[dataItem.docname] = dataItem.data;
+            }
+        }
+
+        return chatMessages;
+    });
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// Store a new Message in the database. 
+// Function returns a Promise with data about the new message when it has been stored. 
+async function addChatMessage(messageText) {
+    const collectionData = {
+        date: Timestamp.fromDate(new Date()),
+        message: messageText,
+        authorname: (currentUser.displayName.length > 0 ? currentUser.displayName : currentUser.email),
+        authorid: currentUser.uid
+    };
+    return dbStoreDocument(db, "chatmeddelande", collectionData);
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// Retrieve up to resultLimit documents from the collectionName collection, sorted by
+// the sortResultBy parameter.
+//  - db is the database handler global.
+//  - collectionName is a string with the name of the Database collection
+//  - sortResultBy is an array where the first element is the name of the field to sort by, and
+//              the second element is either "asc" or "desc" to set the sort order.
+//  - resultLimit is an integer number to limit how many documents to retrieve
+async function dbGetCollectionDocuments(db, collectionName, sortResultBy, resultLimit = 30) {
+    try {
+        const fetchQuery = query(collection(db, collectionName), orderBy(...sortResultBy), limit(resultLimit));
+        const dbDocuments = await getDocs(fetchQuery);
+
+        const resultArray = [];
+        dbDocuments.forEach((doc) => {
+            const resultDoc = {
+                docname: doc.id,
+                data: doc.data()
+            }
+            resultArray.push(resultDoc);
+        });
+        return resultArray;
+    }
+    catch (error) {
+        console.error("Error reading from collection: ", error);
+    }
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// Store a new document in the database.
+//  - db is the database handler global
+//  - collectionName is a string with the name of the Database collection to store the document in.
+//  - collectionData is an object with properties matching the names of the database fields to store in the document
+//  - documentName is the name of the document. Omit to make the database automatically assign a name. 
+async function dbStoreDocument(db, collectionName, collectionData, documentName = null) {
+    try {
+        if (documentName !== null) {
+            return await setDoc(doc(db, collectionName, documentName), collectionData);
+        }
+        else {
+            return await addDoc(collection(db, collectionName), collectionData);
+        }
+    }
+    catch (error) {
+        console.error("Error writing to collection: ", error);
+    }
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// Utility to determine if a text variable has been set and assigned a value.
+function getIsValidText(text) {
+    return ((text !== undefined) && (text !== null) && (text.length !== undefined) && (text.length > 0));
+}
+
+
+export {
+    addChatMessage,
+    getChatMessages,
+    dbGetCollectionDocuments,
+    dbStoreDocument,
+    userLogin,
+    userLogoff,
+    userIsLoggedIn,
+    userUpdateProfile,
+    getCurrentUserName,
+    setUserLoginCallback,
+    setUserLogoffCallback,
+    getCurrentUserProfile,
+    createNewUser
+};
