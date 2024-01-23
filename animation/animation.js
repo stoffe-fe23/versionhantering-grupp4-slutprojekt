4ce30d6ab3aa1f3e568ae643215362d3a76ff688
lunayr <144939306+lunayr@users.noreply.github.com>
2024-01-23 16:24:39 +0100

    luna background animation

---
+++ animation/animation.js
@@ -0,0 +1,29 @@
+const containerAnime = document.querySelector(".container-anime");
+
+for (let i = 0; i <= 100; i++) {
+  const blocksAnime = document.createElement("div");
+  blocksAnime.classList.add("block-anime");
+  containerAnime.appendChild(blocksAnime);
+}
+
+function animateBlocks() {
+  anime({
+    targets: ".block-anime",
+    translateX: function () {
+      return anime.random(-700, 700);
+    },
+    translateY: function () {
+      return anime.random(-500, 500);
+    },
+    scale: function () {
+      return anime.random(1, 5);
+    },
+
+    easing: "linear",
+    duration: 3000,
+    delay: anime.stagger(10),
+    complete: animateBlocks,
+  });
+}
+
+animateBlocks();
