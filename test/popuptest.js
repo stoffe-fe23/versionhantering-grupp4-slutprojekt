3b5326587578bfd148a218fefb36b7d5fbc3feb9
kamyoo <kamy.alinejad@gmail.com>
2024-01-25 01:45:13 +0100

    CSS / Darkmode update

--- test/popuptest.js
+++
@@ -1,9 +0,0 @@
-document.querySelector("#update-user").addEventListener("click", (event) => {
-    if (userIsLoggedIn()) {
-        const inputField = document.querySelector("#change-name-input");
-        inputField.value = getCurrentUserName();
-        inputField.focus();
-        inputField.select();
-        document.querySelector("#change-name-dialog").showModal();
-    }
-});
